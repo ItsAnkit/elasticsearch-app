da5ab6c23ad2fb7c3f26f3a04c8898e800a3adaf
ItsAnkit <ankitsardwal007@gmail.com>
2020-11-21 13:26:30 +0530

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = ['Rahul Dravid', 'Mark ten', 'Dwight Schrute', 'Micky Ricky']
+dobs = [Date.new(1990, 12, 23), Date.new(1990, 05, 23), Date.new(1980, 12, 3), Date.new(1980, 10, 23)]
+addresses = ["Flat - 342, NYC", "Flat-17, Delhi", "Flat-89, Macdy", "Flat-784, KFC"]
+departments = ['Eng', 'Sales', 'Med', 'Product']
+
+Rails.logger.info "--- Seeding started -----"
+
+names.zip(dobs, addresses, departments).each do |name, dob, address, department|
+    next if Employee.find_by_name(name).present?
+    Employee.create(name: name, dob: dob, address: address, department: department)
+end
+
+Rails.logger.info "--- Seeding finished -----"
